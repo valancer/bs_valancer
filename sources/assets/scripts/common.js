fcf4fa51867ed9458781362417a874210cbc1e19
valancer <developer.github@valancer.com>
2016-07-08 04:15:05 +0900

    서비스 아이콘 추가 및 global 영역에 bespin global 홈 버튼 추가

--- sources/assets/scripts/common.js
+++ sources/assets/scripts/common.js
@@ -23,7 +23,6 @@ if (!window.console) {
 })();
 
 
-
 $(document).ready(function(e) {
 	// icheck
 	$('input.icheckbox').iCheck();
@@ -31,9 +30,6 @@ $(document).ready(function(e) {
 
 	// select
 	$('select').selectric({});
-
-	GlobalMenu.init();
-	WarningMessage.init();
 
 	$('.btn-toggle').on('click', function(e) {
 		var $btnToggle = $(this);
@@ -50,98 +46,7 @@ $(document).ready(function(e) {
 
 
 
-/* GlobalMenu */
-var GlobalMenu = (function ($) {
-	var scope,
-		$container,
-		$selectLanguage,
-		$selectCompany,
-		$userInfo,
-		$service,
-		init = function() {
-			$container = $('.global-menu');
-			$selectLanguage = $('#language');
-			$selectCompany = $('#company');
-			$service = $('#service-shortcut');
-			$userInfo = $('#user-info');
-
-
-			initLayout();
-			initEvent();
-		};
-
-	function initLayout() {
-	}
-
-	function initEvent() {
-		$selectLanguage.popup({
-			type: 'tooltip',
-			vertical: 'topedge',
-			offsettop: -16,
-			horizontal: 'leftedge',
-			transition: '0.3s all 0.1s'
-		});
-		$selectCompany.popup({
-			type: 'tooltip',
-			vertical: 'topedge',
-			offsettop: -12,
-			offsetleft: -12,
-			horizontal: 'leftedge',
-			transition: '0.3s all 0.1s'
-		});
-		$service.popup({
-			type: 'tooltip',
-			vertical: 'bottom',
-			offsettop: 50,
-			horizontal: 'center',
-			transition: '0.3s all 0.1s'
-		});
-		$userInfo.popup({
-			type: 'tooltip',
-			vertical: 'bottom',
-			offsettop: 70,
-			horizontal: 'rightedge',
-			transition: '0.3s all 0.1s'
-		});
-	}
-
-	return {
-		init: function() {
-			init();
-		}
-	};
-}(jQuery));
 
 
 
-/* WarningMessage */
-var WarningMessage = (function ($) {
-	var scope,
-		$container,
-		$btnToggleContainer,
-		init = function() {
-			$container = $('.warning-container');
-			$btnToggleContainer = $container.find('.btn-folding');
 
-			initLayout();
-			initEvent();
-		};
-
-	function initLayout() {
-	}
-
-	function initEvent() {
-		// item open / close
-		$btnToggleContainer.on('click', function(e) {
-			e.preventDefault();
-			
-			$container.toggleClass('is-opened');
-		});
-	}
-
-	return {
-		init: function() {
-			init();
-		}
-	};
-}(jQuery));
